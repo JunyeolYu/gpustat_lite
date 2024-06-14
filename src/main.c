2888b54b6a2d0a16d161d2f09d41033bf7496519
JunyeolYu <ryuwhale95@gmail.com>
2024-06-14 12:37:33 +0900

    feat: support for multiple GPU selection (#1)
    - Modify `displayDeviceInfo` function to include GPU_ID
    - Implement `parse_gpu_ids` functions for handling multiple GPU selection
    - Update `main` function to initialize NVML before opt parsing

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 #include <nvml.h>
 #include <ncurses.h>
 
+#define MAX_GPUS 8
+#define MAX_ITEMS 100
+
 void handleError(nvmlReturn_t result) {
     if (NVML_SUCCESS != result) {
         endwin();
@@ -14,7 +17,7 @@ void handleError(nvmlReturn_t result) {
     }
 }
 
-void displayDeviceInfo(nvmlDevice_t device, int index) { //, int cap, int tot_mem) {
+void displayDeviceInfo(nvmlDevice_t device, int id, int index) { //, int cap, int tot_mem) {
     char name[NVML_DEVICE_NAME_BUFFER_SIZE];
     nvmlMemory_t memory;
     nvmlUtilization_t utilization;
@@ -26,7 +29,7 @@ void displayDeviceInfo(nvmlDevice_t device, int index) { //, int cap, int tot_me
     handleError(nvmlDeviceGetPowerUsage(device, &pwr));
 
     mvprintw(index, 0, "G%d | %3u W | %5d MiB | %3u%%\n",
-           index,
+           id,
            pwr/1000,
            //cap,
            (int) (memory.used / 1024 /1024),
@@ -39,17 +42,56 @@ void clearScreen() {
     erase();
 }
 
+int compare_ints(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+void parse_gpu_ids(char* optarg, int* gpus, unsigned int* gpu_count) {
+    char *token;
+    int temp_gpus[MAX_ITEMS];
+    unsigned temp_count = 0;
+
+    // seperate by comma
+    token = strtok(optarg, ",");
+    while (token != NULL && temp_count < MAX_ITEMS) {
+        temp_gpus[temp_count++] = atoi(token);
+        token = strtok(NULL, ",");
+    }
+
+    // remove duplicates
+    *gpu_count = 0;
+    unsigned int i,j;
+    for (i = 0; i < temp_count; i++) {
+        for (j = 0; j < *gpu_count; j++) {
+            if (gpus[j] == temp_gpus[i]) 
+                break;
+        }
+        if (j == *gpu_count) {
+            gpus[(*gpu_count)++] = temp_gpus[i];
+        }
+    }
+
+    qsort(gpus, *gpu_count, sizeof(int), compare_ints);
+}
+
 int main(int argc, char *argv[]) {
     unsigned int device_count, i;
-    nvmlDevice_t device;
+    nvmlDevice_t device[MAX_GPUS];
     int opt;
-    int gpu_id = -1;
+    int gpu_ids[MAX_GPUS];
+    unsigned selected_device_count;
     int interval = 1;
+    bool selected = false;
+
+    handleError(nvmlInit());
+    handleError(nvmlDeviceGetCount(&device_count));
+    selected_device_count = device_count;
 
     while ((opt = getopt(argc, argv, "i:n:h")) != -1) {
         switch (opt) {
             case 'i':
-                gpu_id = atoi(optarg);
+                parse_gpu_ids(optarg, gpu_ids, &selected_device_count);
+                selected = true;
                 break;
             case 'n':
                 interval = atoi(optarg);
@@ -65,15 +107,24 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    handleError(nvmlInit());
-    handleError(nvmlDeviceGetCount(&device_count));
+    if (!selected) {
+        for(i = 0; i < selected_device_count; i++) {
+            gpu_ids[i] = i;
+        }
+    }
 
-    if (gpu_id != -1) {
-        if (gpu_id >= (int)device_count) {
-            fprintf(stderr, "Error: GPU ID %d out of range (0-%d)\n", gpu_id, device_count - 1);
+    for(i = 0; i < selected_device_count; i++) {
+        if (gpu_ids[i] < 0 && gpu_ids[i] >= (int)device_count) {
+            fprintf(stderr, "Error: No GPUs were found. ID %d out of range \
+                                    (0-%d)\n", gpu_ids[i], device_count - 1);
             nvmlShutdown();
             exit(EXIT_FAILURE);
         }
+    }
+
+    // get device handles
+    for(i = 0; i < selected_device_count; i++) {
+        handleError(nvmlDeviceGetHandleByIndex(gpu_ids[i], &device[i]));
     }
 
     initscr();
@@ -83,14 +134,9 @@ int main(int argc, char *argv[]) {
     while (1) {
         clearScreen();
 
-        if (gpu_id == -1) {
-            for (i = 0; i < device_count; i++) {
-                handleError(nvmlDeviceGetHandleByIndex(i, &device));
-                displayDeviceInfo(device, i);
-            }
-        } else {
-            handleError(nvmlDeviceGetHandleByIndex(gpu_id, &device));
-            displayDeviceInfo(device, gpu_id);
+        // print device info line by line
+        for(i = 0; i < selected_device_count; i++) {
+            displayDeviceInfo(device[i], gpu_ids[i], i);
         }
 
         refresh();
